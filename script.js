8d6e1c042d72d972addd0fddda74c1fa81e78fca
SidVadyalam <157451792+SidVadyalam@users.noreply.github.com>
2024-10-29 17:22:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -417,35 +417,33 @@ function locomotive() {
     end: `600% top`,
   });
   
-  
-  
-  gsap.to("#page1",{
-    scrollTrigger:{
-      trigger:`#page1`,
-      start:`top top`,
-      end:`bottom top`,
-      pin:true,
-      scroller:`#main`
-    }
-  })
-  gsap.to("#page2",{
-    scrollTrigger:{
-      trigger:`#page2`,
-      start:`top top`,
-      end:`bottom top`,
-      pin:true,
-      scroller:`#main`
-    }
-  })
-  gsap.to("#page3",{
-    scrollTrigger:{
-      trigger:`#page3`,
-      start:`top top`,
-      end:`bottom top`,
-      pin:true,
-      scroller:`#main`
-    }
-  })
+gsap.to("#page1",{
+scrollTrigger:{
+  trigger:`#page1`,
+  start:`top top`,
+  end:`bottom top`,
+  pin:true,
+  scroller:`#main`
+}
+})
+gsap.to("#page2",{
+scrollTrigger:{
+  trigger:`#page2`,
+  start:`top top`,
+  end:`bottom top`,
+  pin:true,
+  scroller:`#main`
+}
+})
+gsap.to("#page3",{
+scrollTrigger:{
+  trigger:`#page3`,
+  start:`top top`,
+  end:`bottom top`,
+  pin:true,
+  scroller:`#main`
+}
+})
 
 function onAnimationComplete() {
         window.parent.postMessage("animationComplete", "*");
