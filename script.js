a7b46ea1e3d423b199b5b0f309c6d0e0ece5be0e
SidVadyalam <157451792+SidVadyalam@users.noreply.github.com>
2024-10-29 01:30:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -408,6 +408,45 @@ function locomotive() {
     );
   }
 
+  ScrollTrigger.create({
+    trigger: "#page>canvas",
+    pin: true,
+    // markers:true,
+    scroller: `#main`,
+    start: `top top`,
+    end: `600% top`,
+  });
+  
+  
+  
+  gsap.to("#page1",{
+    scrollTrigger:{
+      trigger:`#page1`,
+      start:`top top`,
+      end:`bottom top`,
+      pin:true,
+      scroller:`#main`
+    }
+  })
+  gsap.to("#page2",{
+    scrollTrigger:{
+      trigger:`#page2`,
+      start:`top top`,
+      end:`bottom top`,
+      pin:true,
+      scroller:`#main`
+    }
+  })
+  gsap.to("#page3",{
+    scrollTrigger:{
+      trigger:`#page3`,
+      start:`top top`,
+      end:`bottom top`,
+      pin:true,
+      scroller:`#main`
+    }
+  })
+
 function onAnimationComplete() {
         window.parent.postMessage("animationComplete", "*");
     }
